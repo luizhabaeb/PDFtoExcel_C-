858d6b0415eed0fe6cee1c60a8b1bb9c76ea83c9
Luiz Habaeb <ELISILU@ericsson.com>
2022-06-06 21:46:25 -0300

    Project C#

---
+++ PDFtoExcel/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System.DirectoryServices;
+
+namespace PDFtoExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.Asynchronous = true;
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DirectorySearcher directorySearcher1;    
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PDFtoExcel/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using PdfiumViewer;
+
+namespace PDFtoExcel
+{
+    public partial class Form1 : Form
+    {                
+        //Instanciação do pacote - PDF Viewer - que possiblita a leitura do documento.
+        PdfiumViewer.PdfViewer pdf;
+
+        // Componentes do Programa (design)
+        public Form1()
+        {
+            InitializeComponent();
+            pdf = new PdfViewer();
+            pdf.Width = this.Width - 10;
+            pdf.Height = this.Height - 20;
+            this.Controls.Add(pdf);            
+        }
+
+        // Método para abrir o File Explorer e selecionar o arquivo a ser convertido
+        public void openfile(string filepath)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(filepath);
+            var stream = new System.IO.MemoryStream(bytes);
+            PdfDocument pdfDocument = PdfDocument.Load(stream);
+            pdf.Document = pdfDocument;
+        }
+
+        //função completa: Abrir PDF > Exibir no programa > Converter em EXCEL > Abrir e Salvar o arquivo.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                openfile(dialog.FileName);
+            }                       
+            string pathToPdf = dialog.FileName;
+            string pathToExcel = Path.ChangeExtension(pathToPdf, ".xls");
+            
+
+            // Instânciação do pacote para converter as tabelas de PDF em XLS.
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+
+            // 'true' = o calendário usado, a ordem de classificação das strings e a formatação de datas e números.
+            // 'false' = Ignora os textos e converte apenas os dados que estão tabelados.
+            f.ExcelOptions.ConvertNonTabularDataToSpreadsheet = true;
+
+            // 'true'  = Preserva o layout (template original do PDF)
+            // 'false' = Define 'tabelas' antes do texto (grades)
+            f.ExcelOptions.PreservePageLayout = true;
+
+            // Aqui definimos as informações do sistema de escrita do país (formatação)
+            // Calendário usado; a ordem de classificação das strings e a formatação de datas e números.
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("pt-BR");
+            ci.NumberFormat.NumberDecimalSeparator = ",";
+            ci.NumberFormat.NumberGroupSeparator = ".";
+            f.ExcelOptions.CultureInfo = ci;
+
+            f.OpenPdf(pathToPdf); 
+
+            if (f.PageCount > 0)
+            {
+                int result = f.ToExcel(pathToExcel);
+
+                // Abre o arquivo gerado em Excel.
+                if (result == 0)
+                {                
+                    System.Diagnostics.Process.Start(pathToExcel);           
+
+                }
+            }
+
+        }
+    }
+}
